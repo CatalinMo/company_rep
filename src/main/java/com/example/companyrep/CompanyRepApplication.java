e6d54980be51fcd78d915091c817b93a4d4cbbbb
catalin1998 <catalinmorar98@yahoo.com>
2020-02-12 13:45:10 +0200

    First Commit

---
+++ src/main/java/com/example/companyrep/CompanyRepApplication.java
@@ -0,0 +1,13 @@
+package com.example.companyrep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyRepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyRepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/companyrep/CompanyRepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.companyrep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyRepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
